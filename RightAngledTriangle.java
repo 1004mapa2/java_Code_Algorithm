edf6cd468acad2fd5838886a9b0cc60d85f954ca
phj <1004mapa2@naver.com>
2023-02-15 17:04:18 +0900

    직각삼각형 출력하기

---
+++ RightAngledTriangle.java
@@ -0,0 +1,19 @@
+package codingtest;
+
+import java.util.Scanner;
+
+public class RightAngledTriangle {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+        	for(int j = 0; j < i; j++) {
+        		System.out.print("*");
+        	}
+        	System.out.println("");
+        }
+	}
+}

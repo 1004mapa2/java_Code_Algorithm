0347b69dbeb3d317c25e4dbd28807cee16d93bbd
phj <1004mapa2@naver.com>
2023-02-15 16:40:22 +0900

    배열의 유사도

---
+++ ArraySimilar.java
@@ -0,0 +1,32 @@
+package codingtest;
+
+public class ArraySimilar {
+
+	public static void main(String[] args) {
+
+		String[] s1 = { "a", "b", "c" };
+		String[] s2 = { "com", "b", "d", "p", "c" };
+
+		Solution1 sol = new Solution1();
+		int result = sol.solution(s1, s2);
+		System.out.println(result);
+
+	}
+
+}
+
+class Solution1 {
+	public int solution(String[] s1, String[] s2) {
+
+		int answer = 0;
+
+		for (int i = 0; i < s1.length; i++) {
+			for (int j = 0; j < s2.length; j++) {
+				if (s1[i].equals(s2[j])) {
+					answer++;
+				}
+			}
+		}
+		return answer;
+	}
+}
